50f3966d33805ab4424aab7dfc4bf93970680237
Johan Brichau <johan@yesplan.be>
2018-12-30 14:36:47 +0100

    corrected  and documented simple soilbuddy code and diagram

--- SoilBuddy/firmware/src/moisturesensor.ino
+++ SoilBuddy/firmware/src/moisturesensor.ino
@@ -5,10 +5,8 @@
 
 #define PUMP D4 // Pump power control
 
-#define FLOWPOWER D6
-#define FLOWSENSE D5 // Pin must support interrupt
-
-#define INTERVAL 60*30
+#define INTERVAL 60
+#define DRY_PERCENTAGE_TRESHOLD 50
 
 PowerShield batteryMonitor;
 int sensorValue = 0;
@@ -18,7 +16,6 @@ const int wetValue = 1530;
 double soc = 0;
 int wifi;
 int counter;
-int pulsed;
 
 // Set external antenna (remembered after power off)
 STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
@@ -27,16 +24,13 @@ void setup() {
   batteryMonitor.begin();
   batteryMonitor.quickStart();
   pinMode(SOILPOWER, OUTPUT);
-  pinMode(FLOWPOWER, OUTPUT);
-  pinMode(FLOWSENSE, INPUT_PULLUP);
   pinMode(PUMP,OUTPUT);
-  digitalWrite(FLOWPOWER,LOW);
   digitalWrite(PUMP,LOW);
-  Particle.variable("sensorValue", sensorValue);
+  Particle.variable("sensorValue", sensorValue);  
+  Particle.variable("battery", soc);
+  Particle.variable("percentage", percentage);
   counter = 0;
   sensorValue = 0;
-  pulsed = 0;
-  attachInterrupt(FLOWSENSE, pulse, RISING);
 }
 
 void loop() {
@@ -51,8 +45,8 @@ void loop() {
   if(counter>=3) {
     sensorValue = sensorValue / counter;
     percentage = map(sensorValue, wetValue, dryValue, 100, 0);
-    //if(percentage<50)
-    //  pumpwater();
+    if(percentage<DRY_PERCENTAGE_TRESHOLD)
+      pumpwater();
     Particle.publish("soildata", soil_data(), PRIVATE);
     System.sleep(SLEEP_MODE_DEEP,INTERVAL);
   }
@@ -72,19 +66,8 @@ void measure_soc() {
   soc = batteryMonitor.getSoC();
 }
 
-void pulse() {
-  pulsed++;
-}
-
 void pumpwater() {
-  int previous_pulsed;
-  digitalWrite(FLOWPOWER,HIGH);
   digitalWrite(PUMP,HIGH);
-  do {
-    previous_pulsed = pulsed;
-    delay(10000);
-    Serial.print(pulsed);
-  } while(pulsed > previous_pulsed+100 && pulsed <= 100000);
+  delay(10000);
   digitalWrite(PUMP,LOW);
-  digitalWrite(FLOWPOWER,LOW);
 }
