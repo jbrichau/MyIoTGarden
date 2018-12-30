30f1856d44a0cd166c1cb524f195610dc7651c73
Johan Brichau <johan@yesplan.be>
2018-12-30 18:59:08 +0100

    refactored the code to state-switching: this allows the particle code to run so we can flash the device; also added a waiting period at the end to keep the device online to facilitate this

--- SoilBuddy/firmware/src/moisturesensor.ino
+++ SoilBuddy/firmware/src/moisturesensor.ino
@@ -5,55 +5,125 @@
 
 #define PUMP D4 // Pump power control
 
-#define INTERVAL 60
+#define INTERVAL 60*30
 #define DRY_PERCENTAGE_TRESHOLD 50
+
+#define STATE_START 1
+#define STATE_MEASURING 2
+#define STATE_PUMPING 3
+#define STATE_SENDING 4
+#define STATE_WAITING 5
+
+#define MILLISECONDSONLINE  1000*60*5
 
 PowerShield batteryMonitor;
 int sensorValue = 0;
-int percentage = 0;
+int soilHumidity = 0;
 const int dryValue = 3120;
 const int wetValue = 1530;
 double soc = 0;
-int wifi;
-int counter;
+int wifi = 0;
+int measureCounter;
+int pumpCounter;
+int state;
+unsigned long startTime;
 
 // Set external antenna (remembered after power off)
 STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
 
 void setup() {
+  startTime = millis();
   batteryMonitor.begin();
   batteryMonitor.quickStart();
   pinMode(SOILPOWER, OUTPUT);
   pinMode(PUMP,OUTPUT);
   digitalWrite(PUMP,LOW);
-  Particle.variable("sensorValue", sensorValue);  
   Particle.variable("battery", soc);
-  Particle.variable("percentage", percentage);
-  counter = 0;
+  Particle.variable("soilhumidity", soilHumidity);
+  measureCounter = 0;
+  pumpCounter = 0;
   sensorValue = 0;
+  state = STATE_START;
 }
 
 void loop() {
-  measure_wifi();
-  measure_soc();
-  digitalWrite(SOILPOWER, HIGH);
-  delay(1000);
-  sensorValue += analogRead(SOILSENSE);
-  digitalWrite(SOILPOWER, LOW);
-  delay(5000);
-  counter++;
-  if(counter>=3) {
-    sensorValue = sensorValue / counter;
-    percentage = map(sensorValue, wetValue, dryValue, 100, 0);
-    if(percentage<DRY_PERCENTAGE_TRESHOLD)
-      pumpwater();
-    Particle.publish("soildata", soil_data(), PRIVATE);
-    System.sleep(SLEEP_MODE_DEEP,INTERVAL);
+  switch (state) {
+    case STATE_START:
+      start_soil_measuring();
+      break;
+
+    case STATE_MEASURING:
+      if(measureCounter < 3)
+        measure_soil_iter();
+      else {
+        end_measuring();
+        if(soilHumidity < DRY_PERCENTAGE_TRESHOLD)
+          start_pumping();
+        else
+          start_sending();
+      }
+      break;
+
+    case STATE_PUMPING:
+      if(pumpCounter >= 3) {
+        end_pumping();
+        start_sending();
+      } else pumping_iter();
+      break;
+
+    case STATE_SENDING:
+      measure_wifi();
+      measure_soc();
+      Particle.publish("soildata", soil_data(), PRIVATE);
+      state = STATE_WAITING;
+      break;
+
+    case STATE_WAITING:
+      if(startTime + MILLISECONDSONLINE < millis())
+        System.sleep(SLEEP_MODE_DEEP,INTERVAL);
+      else
+        delay(3000);
   }
 }
 
 String soil_data() {
-  return String::format("{\"sensorValue\":%d, \"percentage\": %d, \"soc\":%f,\"wifi\":%d}",sensorValue,percentage,soc,wifi);
+  return String::format("{\"soilhumidity\": %d, \"soc\":%f,\"wifi\":%d,\"pumped\": %d}",soilHumidity,soc,wifi);
+}
+
+void start_soil_measuring() {
+  state = STATE_MEASURING;
+  digitalWrite(SOILPOWER, HIGH);
+  delay(3000);
+}
+
+void measure_soil_iter() {
+  sensorValue += analogRead(SOILSENSE);
+  measureCounter++;
+  delay(3000);
+}
+
+void end_measuring() {
+  sensorValue = sensorValue / measureCounter;
+  soilHumidity = map(sensorValue, wetValue, dryValue, 100, 0);
+}
+
+void start_pumping() {
+  state = STATE_PUMPING;
+  digitalWrite(PUMP,HIGH);
+}
+
+void pumping_iter() {
+  pumpCounter++;
+  delay(5000);
+}
+
+void end_pumping() {
+  digitalWrite(PUMP,LOW);
+}
+
+void start_sending() {
+  digitalWrite(SOILPOWER, LOW);
+  state = STATE_SENDING;
 }
 
 void measure_wifi() {
@@ -64,10 +134,4 @@ void measure_wifi() {
 
 void measure_soc() {
   soc = batteryMonitor.getSoC();
-}
-
-void pumpwater() {
-  digitalWrite(PUMP,HIGH);
-  delay(10000);
-  digitalWrite(PUMP,LOW);
 }
